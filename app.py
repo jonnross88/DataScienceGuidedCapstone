6c9d2e2ad490b762150e6396845f6195a38bb918
jonnross88 <68479485+jonnross88@users.noreply.github.com>
2023-10-29 01:05:24 -0500

    added more comparativeness to the app.py

--- app.py
+++ app.py
@@ -109,6 +109,8 @@ def predict_increase(features, deltas):
 def format_M(num):
     if abs(num) >= 1_000_000:
         return f"{num/1000000:.1f}M"
+    elif abs(num) >= 1_000:
+        return f"{num/1000:.0f}K"
     else:
         return f"{num:,.1f}"
 
@@ -131,22 +133,22 @@ widget_props = dict(height=60, bar_color=acc_color)
 
 # declare widgets for feature changes
 vertical_drop = pnw.IntSlider(
-    name="Δ Vertical Drop", start=0, end=300, step=50, value=0, **widget_props
+    name="Δ Vertical Drop", start=-300, end=300, step=50, value=0, **widget_props
 )
 delta_runs = pnw.IntSlider(
     name="Δ Number of Runs", start=-10, end=10, step=1, value=0, **widget_props
 )
 delta_chairs = pnw.IntSlider(
-    name="Δ Total Chairs", start=0, end=5, step=1, value=0, **widget_props
+    name="Δ Total Chairs", start=-5, end=5, step=1, value=0, **widget_props
 )
 delta_fastQuads = pnw.IntSlider(
-    name="Δ Fast Quads", start=0, end=2, step=1, value=0, **widget_props
+    name="Δ Fast Quads", start=-2, end=2, step=1, value=0, **widget_props
 )
 delta_SnowMaking_ac = pnw.IntSlider(
-    name="Δ Snow covered acreage", start=0, end=10, step=1, value=0, **widget_props
+    name="Δ Snow covered acreage", start=-5, end=5, step=1, value=0, **widget_props
 )
 delta_longest_run = pnw.FloatSlider(
-    name="Δ Longest Run", start=0.0, end=1.0, step=0.2, value=0.0, **widget_props
+    name="Δ Longest Run", start=-1.0, end=1.0, step=0.1, value=0.0, **widget_props
 )
 
 # Estimates for number of guests and days open
@@ -161,6 +163,11 @@ n_guests = pnw.IntSlider(
 n_days = pnw.IntSlider(
     name="Number of days", start=1, end=10, step=1, value=5, **widget_props
 )
+addl_cost_toggle = pnw.Checkbox(name="Include Add'l Cost", value=False)
+addl_cost = pnw.IntSlider(
+    name="Add'l Cost", value=0, start=0, end=5_000_000, step=10_000, visible=False
+)
+addl_cost_text = pnw.StaticText(name="Additional Cost", value=f"${addl_cost.value:,}")
 
 # dict to map the feature names to the widget display names
 feature_dict = {
@@ -210,15 +217,19 @@ def predicted_increase(
 
 
 # create a function to determine the color of the values based on the value of the ticket change
-def ticket_color(ticket_change):
-    """Returns the color of the ticket change value"""
-    return neg_color if ticket_change < 0 else color3
+# def ticket_color(ticket_change):
+#     """Returns the color of the ticket change value"""
+#     return neg_color if ticket_change < 0 else color3
+# define function to output the revenue estimates
+def income_estimates(guests, days, ticket_change, expenses):
+    new_price = base_price + ticket_change
+    return (new_price * guests * days) - expenses
 
 
-
-def display_features(widgets, ticket_change):
+def display_features(widgets, new_income):
     """Returns a panel Markdown of the widgets values"""
-    value_color = ticket_color(ticket_change)
+    income_change = new_income - last_year_revenue
+    value_color = neg_color if income_change < 0 else color3
     features = [(w.name, w.value) for w in widgets]
     rows = "\n".join(
         [
@@ -226,56 +237,94 @@ def display_features(widgets, ticket_change):
             for f in features
         ]
     )
-    return pn.pane.Markdown(f"{'|'.join(['Feature', 'Value'])}\n|:--|---:|\n{rows}\n")
+    return pn.pane.Markdown(
+        f"{'|'.join(['Contributor', 'Value'])}\n|:--|---:|\n{rows}\n"
+    )
 
 
 def display_features_impacts(widgets, ticket_change):
     """Returns a panel Markdown element of the chosen features values"""
-    value_color = ticket_color(ticket_change)
+    value_color = neg_color if ticket_change < 0 else color3
     # get the widget_name, widget_value, feature_name from the widgets
     features = [
-        (w.name, str(w.value), next(k for k, v in feature_dict.items() if v == w.name))
+        (
+            w.name,
+            f"{w.value:.1f}" if isinstance(w.value, float) else w.value,
+            next(k for k, v in feature_dict.items() if v == w.name),
+            feature_impact(
+                next(k for k, v in feature_dict.items() if v == w.name), float(w.value)
+            ),
+        )
         for w in widgets
     ]
+    total_impact = sum(abs(f[3]) for f in features)
     rows = "\n".join(
         [
-            f"|{f[0]}|<span style='color: {value_color}'>{f[1]}</span>|${feature_impact(f[2], float(f[1])):.2f}|"
+            f"|{f[0]}|<span style='color: {value_color}'>{f[1]}</span>|${f[3]:.2f}|{abs(f[3])/(0.000001+total_impact):.0%}|"
             for f in features
         ]
     )
     return pn.pane.Markdown(
-        f"{'|'.join(['Feature', 'Value', 'Approx. Impact'])}\n|:--|---:|---:|\n{rows}\n"
+        f"{'|'.join(['Feature', 'Change', 'Isolated Impact', '% of Total Impact'])}\n|:--|---:|---:|---:|\n{rows}\n"
     )
 
 
+last_addl_cost = {"value": addl_cost.value}
+
+
+# use the expense toggle to show or hide the expense input
+def expense_toggle_callback(event):
+    if addl_cost_toggle.value:
+        addl_cost.visible = True
+        addl_cost.value = last_addl_cost["value"]
+    else:
+        addl_cost.visible = False
+        last_addl_cost["value"] = addl_cost.value
+        addl_cost.value = 0
+
+
+addl_cost_toggle.param.watch(expense_toggle_callback, "value")
+
+
+def static_text_callback(event):
+    addl_cost_text.value = f"${addl_cost.value:,}"
+
+
+addl_cost.param.watch(static_text_callback, "value")
+
+
 # create separate function for last year comparison
-def last_year_comparison(n_guests, n_days, ticket_change):
+def last_year_comparison(guests, days, ticket_change, expenses=0):
     """Returns a panel Markdown element of the predicted price and last year comparison"""
     new_price = base_price + ticket_change
-    new_revenue = new_price * n_guests * n_days
-    revenue_change = new_revenue - last_year_revenue
+    new_revenue = new_price * guests * days
+    new_income = new_revenue - expenses
+    income_change = new_income - last_year_revenue
 
-    ticket_color = neg_color if ticket_change < 0 else color3
-    revenue_color = neg_color if revenue_change < 0 else color3
+    price_color = neg_color if ticket_change < 0 else color3
+    income_color = neg_color if income_change < 0 else color3
     card_opts = dict(hide_header=True, width=300)
+    num_opts = dict(font_size="40pt")
 
     sub_heads = []
-    sub_heads.append(pn.pane.Markdown("### Relative price change:"))
-    sub_heads.append(pn.pane.Markdown("### Revenue Estimates:"))
+    sub_heads.append(pn.pane.Markdown("### Ticket price:"))
+    sub_heads.append(pn.pane.Markdown("### Aggregates:"))
 
     indicators = []
     delta_ticket = pn.indicators.Number(
         name="Δ Ticket price",
         value=ticket_change,
         format="${value:.2f}",
-        default_color=ticket_color,
+        default_color=price_color,
+        **num_opts,
     )
     indicators.append(delta_ticket)
     new_ticket = pn.indicators.Number(
         name="New ticket price",
         value=new_price,
         format="${value:.2f}",
-        default_color=ticket_color,
+        default_color=price_color,
+        **num_opts,
     )
     indicators.append(new_ticket)
     last_year_ticket = pn.indicators.Number(
@@ -283,28 +332,31 @@ def last_year_comparison(n_guests, n_days, ticket_change):
         value=base_price,
         format="${value:.2f}",
         default_color=minor_color,
+        **num_opts,
     )
     indicators.append(last_year_ticket)
 
     d_revenue = pn.indicators.Number(
-        name="Estimated Δ revenue",
-        value=revenue_change / 1_000_000
-        if abs(revenue_change) >= 1_000_000
-        else revenue_change,
-        format="${value:.1f}M" if abs(revenue_change) >= 1_000_000 else "${value:,.0f}",
-        default_color=revenue_color,
+        name="Estimated Δ Income",
+        value=income_change / 1_000_000
+        if abs(income_change) >= 1_000_000
+        else income_change,
+        format="${value:.1f}M" if abs(income_change) >= 1_000_000 else "${value:,.0f}",
+        default_color=income_color,
+        **num_opts,
     )
     indicators.append(d_revenue)
     revenue = pn.indicators.Number(
-        name="Estimated revenue",
+        name="Estimated Revenue",
         value=new_revenue / 1_000_000 if abs(new_revenue) >= 1_000_000 else new_revenue,
         format="${value:.1f}M" if abs(new_revenue) >= 1_000_000 else "${value:,.0f}",
-        default_color=revenue_color,
+        default_color=income_color,
+        **num_opts,
     )
     indicators.append(revenue)
 
     last_year_revenue_i = pn.indicators.Number(
-        name="Last Year revenue",
+        name="Last Year Revenue",
         value=last_year_revenue / 1_000_000
         if abs(last_year_revenue) >= 1_000_000
         else last_year_revenue,
@@ -312,6 +364,7 @@ def last_year_comparison(n_guests, n_days, ticket_change):
         if abs(last_year_revenue) >= 1_000_000
         else "${value:,.0f}",
         default_color=minor_color,
+        **num_opts,
     )
     indicators.append(last_year_revenue_i)
 
@@ -328,10 +381,10 @@ def last_year_comparison(n_guests, n_days, ticket_change):
 
 
 # get df of last year comparison
-def get_revenue_df(n_guests, n_days, ticket_change):
+def get_revenue_df(guests, days, ticket_change):
     """Get a dataframe of last year's revenue and predicted revenue"""
     new_price = base_price + ticket_change
-    predicted_revenue = new_price * n_guests * n_days
+    predicted_revenue = new_price * guests * days
     # revenue_change = predicted_revenue - last_year_revenue
 
     revenue_df = pd.DataFrame(
@@ -365,6 +418,7 @@ def hbar_callback(df):
         ylabel="",
         invert_axes=True,
         tools=["hover"],
+        active_tools=["box_zoom"],
         toolbar="above",
         xticks=4,
         # ylim=(0, 220_000_000),
@@ -378,7 +432,6 @@ def hbar_callback(df):
         },
     )
     return bars
-
 
 # define the reset button and callback function
 def reset_features(event):
@@ -435,7 +488,11 @@ reactive_predicted_increase = pn.bind(
     delta_longest_run,
 )
 
-reactive_color = pn.bind(ticket_color, reactive_predicted_increase)
+reactive_income_estimates = pn.bind(
+    income_estimates, n_guests, n_days, reactive_predicted_increase, addl_cost
+)
+
+# reactive_color = pn.bind(ticket_color, reactive_predicted_increase)
 
 feature_widgets = [
     delta_fastQuads,
@@ -448,33 +505,55 @@ feature_widgets = [
 
 default_values = {w.name: w.value for w in feature_widgets}
 estimator_widgets = [n_guests, n_days]
+addl_cost_widgets = [addl_cost]
+
 
 # reactive features display
 reactive_features = pn.bind(
     display_features_impacts, feature_widgets, reactive_predicted_increase
 )
 reactive_estimators = pn.bind(
-    display_features, estimator_widgets, reactive_predicted_increase
+    display_features, estimator_widgets, reactive_income_estimates
 )
+reactive_addl_cost = pn.bind(
+    display_features, addl_cost_widgets, reactive_income_estimates
+)
+reactive_pie_chart = pn.bind(pie_chart_callback, feature_widgets)
 
 
 # bound the reactive function to the last year comparison function
 reactive_last_year_comparison = pn.bind(
-    last_year_comparison, n_guests, n_days, reactive_predicted_increase
+    last_year_comparison, n_guests, n_days, reactive_predicted_increase, addl_cost
 )
 reactive_revenue_df = pn.bind(
     get_revenue_df, n_guests, n_days, reactive_predicted_increase
 )
 
 reactive_hbar = pn.bind(hbar_callback, df=reactive_revenue_df)
+
+# Markdown tables in side panel
 features_table_md = pn.Column(
-    pn.panel(pn.pane.Markdown("#### Whatif Features"), width=300),
+    pn.Row(
+        pn.pane.Markdown("#### Whatif Features"),
+        pnw.TooltipIcon(
+            value="""
+            These are the features which have been adjusted from the widgets panel. 
+            Note that the sum of the individual impacts may not equal the total 
+            impact due to interactions between the features."""
+        ),
+        width=300,
+    ),
     reactive_features,
     width=300,
 )
 estimates_table_md = pn.Column(
     pn.panel(pn.pane.Markdown("#### Estimates"), width=300),
     reactive_estimators,
+    width=300,
+)
+addl_cost_table_md = pn.Column(
+    pn.panel(pn.pane.Markdown("#### Additional Cost"), width=300),
+    reactive_addl_cost,
     width=300,
 )
 
@@ -484,8 +563,9 @@ pn.state.kill_all_servers()
 
 logo_path = "./Big Mountain Resort.svg"
 
-md_tables = [features_table_md, estimates_table_md]
+md_tables = [features_table_md, estimates_table_md, addl_cost_table_md]
 
+# Widgets Controls
 w_controls = [
     reset_button,
     pn.Card(
@@ -498,6 +578,8 @@ w_controls = [
         title="Features",
     ),
     pn.Card(n_guests, n_days, title="Estimates"),
+    pn.Card(addl_cost_toggle, addl_cost_text, addl_cost, title="Add'l Cost"),
+    pn.Card(reactive_pie_chart, title="Feature Impacts")
 ]
 # declare floating widget controls panel
 w_floatie = pn.layout.FloatPanel(
